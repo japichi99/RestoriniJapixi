c565d6edad1a26b12b55200a34e3c0ffce27682a
japichi <jos.retamalc@duocuc.cl>
2025-10-17 18:23:51 -0300

    Update Boleta.kt

--- app/src/main/java/com/example/restorinijapixini_joseretamal/model/Boleta.kt
+++ app/src/main/java/com/example/restorinijapixini_joseretamal/model/Boleta.kt
@@ -1,13 +1,18 @@
 package com.example.restorinijapixini_joseretamal.model
 
-class Boleta (
-    val ciente: Cliente,
+class Boleta(
+    val cliente: Cliente,
     val carrito: Carrito,
-    val precioTotal: Double,
-){
-    fun calcularPrecio() : Double{
-        var total = carrito.producto.sumOf { it.producto.precio * it.cantidad }. Double()
+    var precioTotal: Double // Cambié a 'var' para poder modificar el precio total
+) {
+    fun calcularPrecio(): Double {
+        // Calculamos el precio total de manera más clara
+        val total = carrito.producto.sumOf { it.producto.precio * it.cantidad }
+
+        // Asignamos el resultado a precioTotal
         precioTotal = total
+
+        // Retornamos el total calculado
         return total
     }
 }
